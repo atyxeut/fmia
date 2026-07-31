80b374446e5ca431544c33f190ab02d5395c883a
atyxeut <atyxeut@outlook.com>
2026-08-01 04:43:54 +0800

    update license

--- test/runner/math/print_first_10000_humble_numbers.cpp
+++ test/runner/math/print_first_10000_humble_numbers.cpp
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-FileCopyrightText: 2026 atyxeut <atyxeut@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 import test.fmia.math.sequence.smooth_number;
 
